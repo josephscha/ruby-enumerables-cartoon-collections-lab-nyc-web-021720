8a47951428e9cb3dc78033a5fe6866daadba428e
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 01:56:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def long_planeteer_calls(source)# code an argument here
   # Your code here
-  source.filter
+  source.include? do {|source| source.length > 4}
 end
 
 def find_the_cheese# code an argument here
