9529d767188051b41568a9f1dc870f9c91ae1ef4
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 01:52:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(source)# code an argument here
   # Your code here
+  index = 0 
+  while index < source.length 
 end
 
 def summon_captain_planet# code an argument here
