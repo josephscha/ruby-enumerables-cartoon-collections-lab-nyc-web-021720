3bf67c6ab6e343dd946b8331874b12ae208731d2
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 01:56:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def summon_captain_planet(source)# code an argument here
   # Your code here
-  return source.map{|i| i.capitalize + "!"}
+  return source.collect{|i| i.capitalize + "!"}
 end
 
 def long_planeteer_calls(source)# code an argument here
