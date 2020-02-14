5ad5790d150ddb78797c7535096208bf5a7376ed
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 02:00:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(source)# code an argument here
   source.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(source)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  source.find {|type| cheese_types.include?(type)}
 end
