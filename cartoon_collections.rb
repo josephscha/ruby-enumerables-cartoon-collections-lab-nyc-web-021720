f4a2750411763e5290a5abddec6b8428acefc438
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 02:00:57 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(source)# code an argument here
   # Your code here
+  index = 0 
+  while index < source.length 
+  puts "#{index + 1}. #{source[index]}"
+  index += 1 
+end 
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(source)# code an argument here
   # Your code here
+  return source.collect{|i| i.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(source)# code an argument here
   # Your code here
+  source.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(source)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  source.find {|i| cheese_types.include?(i)}
 end
